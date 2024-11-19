ba79ed48be13db9e7b0c297aef16d8f30beb673c
tatiane-mendes <79422351+tatiane-mendes@users.noreply.github.com>
2024-11-19 08:51:56 -0500

    Login to consume API

---
+++ src/lib/axios.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  //baseURL: 'https://casamaniocabe-okmn.onrender.com/api/v1',
+  baseURL: 'http://localhost:10000/api/v1',
+  timeout: 60000,
+  headers: {
+    'Accept': 'application/json'
+  }
+});
+
+axiosInstance.interceptors.response.use((response) => response,
+  (error) => { 
+    return Promise.reject((error.response && error.response.data) || 'Something went wrong'); 
+  });
+
+axiosInstance.interceptors.request.use(function (config) {
+  let user = JSON.parse(localStorage.getItem('user'));
+    
+  if(user?.access_token != null && user?.access_token != undefined){
+      config.headers.Authorization = user.access_token;
+  };
+
+  return config;
+});
+
+export default axiosInstance;
---
+++ src/services/authorization-service.js
@@ -0,0 +1,82 @@
+import axios from '../lib/axios';
+
+class AuthorizationService {
+    setAxiosInterceptors = ({ onLogout }) => {
+      axios.interceptors.response.use(
+        (response) => response,
+        (error) => {
+          if (error.response && error.response.status === 401) {
+            this.setSession(null);
+            this.setUser(null);
+  
+            if (onLogout) {
+              onLogout();
+            }
+          }
+  
+          return Promise.reject(error);
+        }
+      );
+    };
+  
+    loginWithPassword = (login, password) => {
+      const data = ({
+        'email': login,
+        'password': password
+      });
+  
+      const headers = {
+        'content-type': 'application/json'
+      };
+  
+      return axios.post('/authorization/login', data, { headers: headers })
+        .then((response) => {
+                  
+          if (response.data) {
+            this.setSession(response.data.access_token);
+            this.setUser(response.data.user);
+
+            return response.data;
+          } else {
+            return response.data;
+          }
+        })
+        .catch((error) => {
+          return Promise.reject(error);
+        });
+    };
+  
+    logout = () => {
+      this.setSession(null);
+      this.setUser(null);
+    }
+  
+    setSession = (accessToken) => {
+      if (accessToken) {
+        localStorage.setItem('accessToken', accessToken);
+        axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+      } else {
+        localStorage.removeItem('accessToken');
+        delete axios.defaults.headers.common.Authorization;
+      }
+    }
+  
+    getAccessToken = () => localStorage.getItem('accessToken');
+  
+    setUser = (user) => { 
+      if (user) {
+        localStorage.setItem('user', JSON.stringify(user));
+      } else {
+        localStorage.removeItem('user');
+      }
+    };
+  
+    getUser = () => {
+      let appUserData = window.localStorage.getItem('user');
+      return JSON.parse(appUserData);
+    };
+  }
+  
+  const authorizationService = new AuthorizationService();
+  
+  export default authorizationService;
